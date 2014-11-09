d7c3c8478868e935c8a901bf244d126776734a32
Matthew Bourque <matthewkbourque@gmail.com>
2014-11-09 15:52:59 -0500

    Added attributes and methods to Process class.

---
+++ Process.java
@@ -0,0 +1,29 @@
+public class Process {
+
+	int pid;
+	double burstTime;
+	double executionTime;
+	double waitingTime;
+	double turnaroundTime;
+
+	public void Process() {
+		/*
+		 * Constructor method.
+		*/
+
+		pid = 0;
+		burstTime = 0.0;
+		executionTime = 0.0;
+		waitingTime = 0.0;
+		turnaroundTime = 0.0;
+	}
+
+	public void start(double timeQuantum) {
+		/*
+		 * Executes the process.
+		*/
+
+		
+
+	}
+}
